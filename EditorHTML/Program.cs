c777e183d154d0795cfa63bcda54228e2eaeb39d
mathmenegon <menegon775@gmail.com>
2025-09-15 22:30:10 -0300

    Push completo

--- EditorHTML/Program.cs
+++ EditorHTML/Program.cs
@@ -1,1 +1,3 @@
-﻿
+﻿using EditorHTML;
+
+Menu.Show();
---
+++ EditorHTML/Viewer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EditorHTML
+{
+    internal class Viewer
+    {
+        public static void Show(string text)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Console.WriteLine("MODO VISUALIZAÇÃO");
+            Console.WriteLine("=============");
+            Console.WriteLine(text);
+            Console.ReadKey();
+            Menu.Show();
+        }
+
+        public static void Replace(string text)
+        {
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            var words = text.Split(' ');
+
+            for (var i = 0; i<= words.Length; i++)
+            {
+                if (strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(
+                            words[i].Substring(
+                                words[i].IndexOf('>') + 1,
+                                (
+                                    (words[i].IndexOf('<') -1) -
+                                    words[i].LastIndexOf('>')
+                                )
+                            )
+                        );
+                    Console.Write(" ");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i] + " ");
+                }
+            }
+        }
+    }
+}
